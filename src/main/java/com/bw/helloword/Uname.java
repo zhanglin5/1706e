b3c4bf6515a299608ed154f74d645d4e9e2d4a7b
zhanglin <zhanglin@qq.com>
2019-10-11 13:47:50 +0800

    创建学生属性

---
+++ src/main/java/com/bw/helloword/Uname.java
@@ -0,0 +1,15 @@
+package com.bw.helloword;
+
+public class Uname {
+
+	private  String numae;
+
+	public String getNumae() {
+		return numae;
+	}
+
+	public void setNumae(String numae) {
+		this.numae = numae;
+	}
+	
+}
